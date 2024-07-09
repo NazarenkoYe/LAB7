3e8e917524307353c4007b853a51a49f59333c33
NazarenkoYe <egornazarenko11@gmail.com>
2024-07-09 19:31:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int circleintersection(int x1, int y1, int r1, int x2, int y2, int r2) {
-    float distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    double distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     if (distance > r1 + r2) {
         return 0;
     } else if (distance == 0 && r1 == r2) {
@@ -17,16 +17,15 @@ int circleintersection(int x1, int y1, int r1, int x2, int y2, int r2) {
 }
 int main() {
     int x1, y1, r1, x2, y2, r2;
-    printf("Введіть координати та радіус першого кола (x1, y1, r1): ");
+    printf("Enter the coordinates and radius of the first circle (x1, y1, r1): ");
     scanf("%d %d %d", &x1, &y1, &r1);
-    printf("Введіть координати та радіус другого кола (x2, y2, r2): ");
+    printf("Enter the coordinates and radius of the second circle (x2, y2, r2): ");
     scanf("%d %d %d", &x2, &y2, &r2);
     int intersectionpoints = circleintersection(x1, y1, r1, x2, y2, r2);
     if (intersectionpoints == -1) {
-        printf("Кола співпадають, безліч точок перетину\n");
+        printf("The circles coincide, infinite intersection points\n");
     } else {
-        printf("Кількість точок перетину: %d\n", intersectionpoints);
+        printf("Number of intersection points: %d\n", intersectionpoints);
     }
     return 0;
 }
-
